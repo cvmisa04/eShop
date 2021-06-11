bfd471541e36d3be8c29b22d052e0a0f9f75e674
cvmisa04 <61522606+cvmisa04@users.noreply.github.com>
2021-06-11 16:21:14 +0200

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -28,7 +28,7 @@ app.use('/api/v1', user)
 app.use('/api/v1',order)
 app.use('/api/v1',payment)
 
-if(process.env.NODE_ENV === 'PRODUCTION'){
+if(process.env.NODE_ENV === 'DEVELOPMENT'){
     app.use(express.static(path.join(__dirname,'../frontend/build')))
 
     app.get('*',(req,res)=>{
